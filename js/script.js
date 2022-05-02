91532b5712dfce76a2335837dd2af94fd14e4847
Bayarsaikhan12 <102643818+Bayarsaikhan12@users.noreply.github.com>
2022-05-02 12:34:40 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let menu = document.querySelector('#menu-bars');
+let header = document.querySelector('#header');
+
+menu.onclick = () =>{
+	menu.classlist.toggle('fa-times');
+	header.classlist.toggle('active');
+}
+
+menu.onscroll = () =>{
+	menu.classlist.remove('fa-times');
+	header.classlist.remove('active');
+}
+
+let cursor1=document.querySelector('.cursor1');
+let cursor2=document.querySelector('.cursor2');
+
+window.onmousemove = (e) =>{
+	cursor1.style.top = e.pageY + 'px';
+	cursor1.style.left = e.pageX + 'px';
+	cursor2.style.top = e.pageY + 'px';
+	cursor2.style.left = e.pageX + 'px';
+}
+document.querySelectorAll('a').forEach(links =>{
+    
+    links.onmousemove = () =>{
+    	cursor1.classlist.add('active');
+    	cursor2.classlist.add('active');
+    }
+
+    links.onmousemove = () =>{
+    	cursor1.classlist.remove('active');
+    	cursor2.classlist.remove('active');
+    }
+});
